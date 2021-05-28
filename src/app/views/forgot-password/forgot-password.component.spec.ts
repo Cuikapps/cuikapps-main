69325a2efbe7f76f81345387a71dc910864aa10d
Amir-Afkhami <70760163+Amir-Afkhami@users.noreply.github.com>
2021-05-28 11:27:46 -0500

    Some unit testing complete

--- src/app/views/forgot-password/forgot-password.component.spec.ts
+++ src/app/views/forgot-password/forgot-password.component.spec.ts
@@ -1,23 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { AuthService } from 'src/app/services/auth.service';
 
 import { ForgotPasswordComponent } from './forgot-password.component';
 
 describe('ForgotPasswordComponent', () => {
   let component: ForgotPasswordComponent;
   let fixture: ComponentFixture<ForgotPasswordComponent>;
+  let testBedAuth: AuthService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ForgotPasswordComponent ]
-    })
-    .compileComponents();
+      declarations: [ForgotPasswordComponent],
+      providers: [AuthService],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ForgotPasswordComponent);
     component = fixture.componentInstance;
+    testBedAuth = TestBed.inject(AuthService);
     fixture.detectChanges();
   });
+
+  it('should inject services', inject([AuthService], (auth: AuthService) => {
+    expect(auth).toBe(testBedAuth);
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/app/views/news/change-log/change-log.component.spec.ts
+++ src/app/views/news/change-log/change-log.component.spec.ts
@@ -1,25 +1,52 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 
 import { ChangeLogComponent } from './change-log.component';
+import { changelogs } from '../../../data/changelogs.json';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('ChangeLogComponent', () => {
   let component: ChangeLogComponent;
   let fixture: ComponentFixture<ChangeLogComponent>;
+  let testBedActivatedRoute: ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChangeLogComponent ]
-    })
-    .compileComponents();
+      declarations: [ChangeLogComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of({
+              get: (v: string) => {
+                return '0';
+              },
+            }),
+          },
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ChangeLogComponent);
     component = fixture.componentInstance;
+    testBedActivatedRoute = TestBed.inject(ActivatedRoute);
     fixture.detectChanges();
   });
 
+  it('should inject services', inject(
+    [ActivatedRoute],
+    (activatedRoute: ActivatedRoute) => {
+      expect(activatedRoute).toBe(testBedActivatedRoute);
+    }
+  ));
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#typeChangeLogs should equal the raw json changelog', () => {
+    expect(component.typedChangeLogs).toEqual(changelogs);
   });
 });
