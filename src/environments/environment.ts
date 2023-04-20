ca926de311615c6e31fa19aad67795c304d470a0
Amir-Afkhami <70760163+Amir-Afkhami@users.noreply.github.com>
2023-04-19 23:08:17 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyA6jV-NqU4Xmlu1p272LN6o4anjNnZgkeE',
-    authDomain: 'cuikapps.firebaseapp.com',
-    databaseURL: 'https://cuikapps-users.firebaseio.com',
-    projectId: 'cuikapps',
-    storageBucket: 'cuikapps.appspot.com',
-    messagingSenderId: '256764122512',
-    appId: '1:256764122512:web:3660eb49b1ae3131fb0f15',
-    measurementId: 'G-01QT5S16K8',
-  },
 };
 
 /*
